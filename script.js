38e595d113e1cca26652c93e30c8434e490cc0da
Bartek-create <wisniewskib.86@tlen.pl>
2025-04-13 21:43:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-// Logika aplikacji
-console.log('Realne dane rynków załadowane');
+function loadData() {
+  fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+      const output = document.getElementById('output');
+      output.innerHTML = '';
+      data.forEach(market => {
+        const div = document.createElement('div');
+        div.innerHTML = `<strong>${market.market}</strong><br>
+          Trend W1: ${market.trend}<br>
+          Sezonowość: ${market.seasonality}<br>
+          COT: ${market.cot.direction} ${market.cot.value}`;
+        output.appendChild(div);
+      });
+    })
+    .catch(err => {
+      document.getElementById('output').innerHTML = 'Błąd ładowania danych.';
+      console.error(err);
+    });
+}
